4d30b0b086a04a7afc1f252eb980174df5e22a0d
Aromal Anil <aromalanilkannan@gmail.com>
2020-04-18 21:40:29 +0530

    🚩 Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Quote from "./components/Quote";
+import TweetButton from "./components/TweetButton.js";
+import "./App.css";
 
 function App() {
+  const [jsonResponse, setJsonResponse] = useState({
+    content: "Loading...",
+    author: "",
+  });
+
+  const fetchQuote = async () => {
+    let response = await (await fetch("https://api.quotable.io/random")).json();
+    response.content=response.content.replace(/[;]/g, "")
+    if(response.content.length>240){
+      console.log('Content exceeds character limit');
+      fetchQuote()
+    }
+    else{
+      setJsonResponse(response);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  },[]);
+
+  const getTweetContent = () =>
+    `"${jsonResponse.content}" are the famous words of ${jsonResponse.author}.
+For more quotes visit ${window.location.hostname} made by  @AromalAnil5`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="quote-box">
+        <Quote content={jsonResponse.content} author={jsonResponse.author} />
+        <div className="bottom-row">
+          <button id="new-quote" onClick={fetchQuote} className="btn">
+            New Quote
+          </button>
+          <TweetButton content={getTweetContent()} />
+        </div>
+      </div>
+    </main>
   );
 }
 
